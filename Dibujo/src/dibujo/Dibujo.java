e639efa853b728cded3af4e0771ef14b9ebe3513
ANDRÉ <ANDR@192.168.0.114>
2019-07-18 00:26:33 -0600

    Original

---
+++ Dibujo/src/dibujo/Dibujo.java
@@ -0,0 +1,29 @@
+
+package dibujo;
+
+public class Dibujo {
+
+    public static void main(String[] args) {
+        System.out.print("     AUTOMOVIL\n");
+            for(int j=0;j<=6;j++){
+            System.out.print(" *");
+        }
+           for(int j=0;j<=5;j++){
+            System.out.print(" ");
+        }
+           
+           System.out.println("");
+            
+                for(int k=0;k<=8;k++){
+            System.out.print(" *");    
+        }
+            
+                 System.out.println("");
+            for(int k=0;k<=8;k++){
+            System.out.print(" *");    
+        }
+            System.out.print("\n   O       O\n");
+            
+        
+    }
+   }
