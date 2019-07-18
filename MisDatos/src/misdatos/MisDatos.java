a57614add975432fd3633ea20a14213665600b88
ANDRÉ <ANDR@192.168.0.114>
2019-07-17 22:01:07 -0600

    Original

---
+++ MisDatos/src/misdatos/MisDatos.java
@@ -0,0 +1,14 @@
+
+package misdatos;
+
+
+public class MisDatos {
+
+   
+    public static void main(String[] args) {
+       System.out.println("Helton André Najarro Quiñonez");
+       System.out.println("Tel: Aldea El Jicaro, Yupiltepeque, Jutiapa");
+       System.out.println("Telefono: 5910 1923");
+    }
+    
+}
