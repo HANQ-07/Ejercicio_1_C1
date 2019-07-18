d806afae201a79353d83f0641e86a6ae3ac9b4ff
ANDRÉ <ANDR@192.168.0.114>
2019-07-17 23:30:40 -0600

    Original

---
+++ Piramide/src/piramide/Piramide.java
@@ -0,0 +1,21 @@
+
+package piramide;
+
+
+public class Piramide {
+
+    public static void main(String[] args) {
+      
+        for(int i=0;i<9;i++){
+    for(int j=9-i;j>0;j--){
+        System.out.print(" ");
+    }
+    for(int j=0;j<i;j++){
+        System.out.print(" *");
+    }
+    System.out.println("");
+    
+}  
+    }
+    
+}
